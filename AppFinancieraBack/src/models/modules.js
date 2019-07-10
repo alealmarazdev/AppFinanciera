2e025eb0256c011638b174198abc96a7395f93bb
vanguardia8a <alochernandez@esimez.mx>
2019-07-10 16:37:53 -0500

    modelo de los mudulos del juego

---
+++ AppFinancieraBack/src/models/modules.js
@@ -0,0 +1,32 @@
+const {Schema, model}=require('mongoose');
+
+const moduleSchema = new Schema({
+    name:{
+        type:String,
+        minlength: 3,
+        maxlength: 50,
+        required: true
+      },
+
+      icon:{
+          type: String,
+          required: true
+      },
+      
+      description:{
+          type: String,
+          minlength: 10,
+          maxlength: 50000,
+          required: true
+      },
+      
+      requeriments:{
+          type: Array,
+          default: []
+      }
+})
+
+module.exports={
+    Schema: moduleSchema,
+    model: model('Modules', moduleSchema)
+}
