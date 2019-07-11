74839a80c0ae914de8ee252d869a44f04be1dc53
vanguardia8a <alochernandez@esimez.mx>
2019-07-11 16:08:36 -0500

    libreria de seguridad para encriptar datos sensibles

--- AppFinancieraBack/src/lib/bcrypt.js
+++ AppFinancieraBack/src/lib/bcrypt.js
@@ -1,21 +1,20 @@
-/* EJEMPLO */
-const bcrypt = require('bcrypt')
+const bcrypt=require('bcrypt');
 
-const hash = (password) => new Promise((resolve, reject) => {
-  bcrypt.hash(password, 10, (error, hash) => {
-    if (error) return reject(error)
-    resolve(hash)
+const hash = (password)=> new Promise((resolve, reject)=>{
+  bcrypt.hash(password,10, (error,hash)=>{
+    if(error) return reject(error)
+    resolve (hash)
   })
 })
 
-const compare = (password, hash) => new Promise((resolve, reject) => {
-  bcrypt.hash(password, hash, (error, same) => {
-    if (error) return reject(error)
-    resolve(same)
+const compare = (password, hash)=> new Promise ((resolve, reject)=>{
+  bcrypt.compare(password,hash,(error,same)=>{
+    if(error) return reject(error)
+    resolve (same)
   })
 })
 
-module.exports = {
+module.exports={
   hash,
   compare
 }
