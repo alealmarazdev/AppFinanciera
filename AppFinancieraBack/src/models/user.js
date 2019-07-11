9993c0287e83b72907ec0a55bf07991fd16b9a3b
devcharles <charlesipn@gmail.com>
2019-07-10 20:15:36 -0500

    se agrego role en usuario

--- AppFinancieraBack/src/models/user.js
+++ AppFinancieraBack/src/models/user.js
@@ -75,6 +75,15 @@ const userSchema = new Schema({
   score:{
     type: Number,
     default:0
+  },
+
+  role: {
+    type: String,
+    enum: [
+      'user',
+      'admin'
+    ],
+    default: 'user'
   }
   
 })
