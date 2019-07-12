dfc923352bb30d0aea94adb82b1a604589380896
alekyrz <alejandra.almaraz23@gmail.com>
2019-07-12 18:31:04 -0500

    [ADD]blog

---
+++ AppFinancieraFront/src/components/Blogcomponents/PrincipalCardLeft/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import JuntaDeNegocio from '../../../asset/image/juntadenegocios.jpg'
+/* import styles from './index.module.css'; */
+
+
+
+function PrincipalCardLeft(){
+
+    return (
+    <div className="w-50 mb-1"> 
+        <div class="card mb-3">
+            <img src={JuntaDeNegocio} class="card-img-top" alt="..."/>
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                </p>
+            </div>
+      </div>
+    </div>
+)
+}
+
+
+export default PrincipalCardLeft
+
