cf902f495867d0752d73cfc67496ce87baf2efbc
alekyrz <alejandra.almaraz23@gmail.com>
2019-07-13 00:59:15 -0500

    [ADD]tuckgame

--- AppFinancieraFront/src/components/TransportGame/index.js
+++ AppFinancieraFront/src/components/TransportGame/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+import styles from './index.module.css'
+
+import Truck from '../../asset/image/truck.png'
+import  QuestionGame from '../QuestionGame/index'
+
 function TransportGame () {
  
      return (
-      <div className="mx-auto d-table my-5">
-          
+      <div className={styles.principalContainerLogo}>
+        <h5>hola</h5>
+        <h6>que pasa?</h6>
+        <div className={styles.road}>
+            <img src={Truck} alt="camion en movimiento" className={styles.truck}/> 
+        </div>
+        
+
+        <QuestionGame />
         
       </div>
     )
