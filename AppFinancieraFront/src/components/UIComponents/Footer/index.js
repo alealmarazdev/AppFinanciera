d440a3d6cb5fe6337a0784904c703fac4517a4ba
alekyrz <alejandra.almaraz23@gmail.com>
2019-07-15 22:01:25 -0500

    [FIX]footer

--- AppFinancieraFront/src/components/UIComponents/Footer/index.js
+++ AppFinancieraFront/src/components/UIComponents/Footer/index.js
@@ -24,13 +24,13 @@ function Footer() {
       {/* <div class="card-header">
         Featured
       </div> */}
-      <div className={`d-flex`}>
-        <div className={`card-body col-sm-3`}>
+      <div className={`row d-flex`}>
+        <div className={`col-12 card-body col-sm-3`}>
           <h5 className={`card-title  ${styles.colorCyan}`}>Suscribete a nuestro Nesletter</h5>
           <p  className={`card-text  ${styles.colorCyan}`}>With supporting text below as a natural lead-in to additional content.</p>
           <ButtonFullWidth class="btn btn-primary" className={`${styles.ButonSize}`} Title="Jugar ahora!" handleClick={() => { toggleModal(true) }} />
         </div>
-        <div className={`card-body col-sm-3`}>
+        <div className={`col-12 card-body col-sm-3`}>
           <p className={`card-text py-0 my-0 ${styles.colorCyan}`}>CONTACTO.</p>
           <p className={`card-text py-0 my-0  ${styles.colorCyan}`}>SOBRE NOSOTROS.</p>
           <p className={`card-text py-0 my-0  ${styles.colorCyan}`}>APRENDE.</p>
@@ -38,12 +38,12 @@ function Footer() {
           <p className={`card-text py-0 my-0  ${styles.colorCyan}`}>JUEGA.</p>
           <p className={`card-text py-0 my-0 ${styles.colorCyan}`}>FAQ.</p>
         </div>
-        <div className={`card-body col-sm-3`}>
+        <div className={`col-12 card-body col-sm-3`}>
         <p class={`card-text py-0 ${styles.colorCyan}`}>POLITICA DE PRIVACIDAD.</p>
           <p class={`card-text py-0 ${styles.colorCyan}`}>TERMINOS Y CONDICIONES.</p>
           <p class={`card-text py-0 ${styles.colorCyan}`}>POLITICA DE DATOS.</p>
         </div>
-        <div className={`card-body col-sm-3`}>
+        <div className={`col-12 card-body col-sm-3`}>
       
           <p class={`card-text py-0 ${styles.colorCyan}`}>Calle Tonala numero 10</p>
           <p class={`card-text py-0 ${styles.colorCyan}`}>Colonia Roma</p>
