b7158bae66ffbd891fb9433082580c4e02fdebc5
alekyrz <alejandra.almaraz23@gmail.com>
2019-07-16 22:39:32 -0500

    [check]port

--- AppFinancieraFront/src/components/UIComponents/Chatbot/index.js
+++ AppFinancieraFront/src/components/UIComponents/Chatbot/index.js
@@ -30,7 +30,7 @@ function Chatbot() {
 
     sound.play();
 
-    const apiUrl = `https://hack-store-smart-sable.mybluemix.net/sendMessage?message=${state.question}`
+    const apiUrl = `https://fishnance-shy-raven.mybluemix.net/sendMessage?message=${state.question}`
 
     const apiCall = await fetch(apiUrl, {
       method: 'GET',
