7b22c5dd4f014ab94f0667ed5cc77a8e44a56447
alekyrz <alejandra.almaraz23@gmail.com>
2019-07-18 13:34:52 -0500

    [FINISH]game5

--- AppFinancieraFront/src/components/MatchGame/index.js
+++ AppFinancieraFront/src/components/MatchGame/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import styles from './index.module.css';
-import Fish from '../../asset/image/fish1.png'
+import Arrendamiento from '../../asset/image/arrendamiento.svg';
+import Concierto from '../../asset/image/concierto.svg';
+import Compraventa from '../../asset/image/compraventa.svg';
+import Servicios from '../../asset/image/servicios.svg';
+
 function MatchGame () {
  
      return (
-      <div className="mx-auto d-table my-5">
-          <h4 className="mb-3">¿Qué es un ingreso?</h4>
+      <div className="mx-auto my-5">
+          <h4 className="mb-3">¿Cual es el impuesto que grava las siguientes imagenes?</h4>
+          <div className="d-flex"> 
           <div class="input-group mb-3">
             <div class="input-group-prepend mr-2">
                 <div class="input-group-text">
                     <input type="checkbox" aria-label="Checkbox for following text input " />
                 </div>
             </div>
-            <label >Es todo objeto que ingresa al haber patrimonial</label>
+            <label >Impuesto al Valor Agregado</label>
             </div>
             <div class="input-group mb-3">
             <div class="input-group-prepend mr-2">
@@ -20,7 +25,7 @@ function MatchGame () {
                     <input type="checkbox" aria-label="Checkbox for following text input"/>
                 </div>
             </div>
-            <label>Es todo objeto que ingresa al haber patrimonial</label>
+            <label>Tenencia</label>
             </div>
             <div class="input-group mb-3">
             <div class="input-group-prepend mr-2">
@@ -28,7 +33,7 @@ function MatchGame () {
                     <input type="checkbox" aria-label="Checkbox for following text input"/>
                 </div>
             </div>
-            <label>Es todo objeto que ingresa al haber patrimonial</label>
+            <label>Nomina</label>
             </div>
             <div class="input-group mb-3">
             <div class="input-group-prepend mr-2">
@@ -36,22 +41,36 @@ function MatchGame () {
                     <input type="checkbox" aria-label="Checkbox for following text input"/>
                 </div>
             </div>
-            <label>Es todo objeto que ingresa al haber patrimonial</label>
+            <label>Predial</label>
             </div>
-        
-            <div className={`${styles.bgPrincipal} col-sm-7`} > 
-                <img src={Fish}  alt="pez en movimiento" className={styles.fish}/>
-            </div>
-            <div className={`${styles.bgPrincipal} col-sm-7`} > 
-                <img src={Fish}  alt="pez en movimiento" className={styles.fish}/>
-            </div>
-            <div className={`${styles.bgPrincipal} col-sm-7`} > 
-                <img src={Fish}  alt="pez en movimiento" className={styles.fish}/>
-            </div>
-            <div className={`${styles.bgPrincipal} col-sm-7`} > 
-                <img src={Fish}  alt="pez en movimiento" className={styles.fish}/>
-            </div>
+        </div>
 
+        <div className="d-flex justify-content-around mx-5"> 
+            <div className={` col-sm-3 d-flex justify-content-center`} > 
+                <div> 
+                    <img src={Arrendamiento}  alt="impuesto en movimiento" className={styles.tax1}/>
+                    <p className={`${styles.tax1} text-center`}>Arrendamiento</p>
+                </div>
+            </div>
+            <div className={`col-sm-3 d-flex justify-content-center`} > 
+                <div> 
+                    <img src={Concierto}  alt="impueso en movimiento" className={styles.tax2}/>
+                    <p className={`${styles.tax2} text-center`}>Conciertos</p>
+                </div>
+            </div>
+            <div className={`col-sm-3 d-flex justify-content-center`} > 
+                <div> 
+                    <img src={Compraventa}  alt="impuesto en movimiento" className={styles.tax3}/>
+                    <p className={`${styles.tax3} text-center`}>Venta de bienes</p>
+                </div>
+            </div>
+            <div className={`col-sm-3 d-flex justify-content-center`} > 
+             <div>
+                <img src={Servicios}  alt="impuesto en movimiento" className={styles.tax4}/>
+                <p className={`${styles.tax4} text-center`}>Prestacion de servicios</p>
+                </div>
+            </div>
+            </div>
       </div>
     )
 }
